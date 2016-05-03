4cbd497ebc37e2c31a53f25a49e0ed7a528d2089
贺梦洁 <2372621687@qq.com>
2016-05-03 16:29:23 +0800

    创建项目

---
+++ ClickWebImageAmplification/ClickWebImageAmplification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClickWebImageAmplification
+//
+//  Created by MJHee on 16/5/3.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClickWebImageAmplification/ClickWebImageAmplification/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ClickWebImageAmplification
+//
+//  Created by MJHee on 16/5/3.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
